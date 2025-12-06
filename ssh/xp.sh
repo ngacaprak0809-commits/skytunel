012c1294e53b2bad6f64ebc92eb08274fcac9abf
Caliph Atibrata <admin@caliphdev.com>
2025-12-07 04:24:39 +0700

    Refactor user auto-removal logic in xp.sh
    Refactor auto-remove logic for VMess, VLESS, and Trojan users. Improved date handling and streamlined user removal from config.json.

--- ssh/xp.sh
+++ ssh/xp.sh
@@ -4,91 +4,49 @@ EXPIRED_FILE="/etc/xray/expired.txt"
 mkdir -p /etc/xray
 touch "$EXPIRED_FILE"
 
-##----- Auto Remove Vmess
-data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-    exp=$(grep -w "^### $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-    d1=$(date -d "$exp" +%s)
+now=$(date +"%Y-%m-%d")
+
+############################
+# AUTO REMOVE VMESS (###)
+############################
+grep -E '^### ' /etc/xray/config.json | while read -r marker user exp; do
+    # marker = "###", user = nama, exp = yyyy-mm-dd
+    [ -z "$user" ] && continue
+    [ -z "$exp" ] && continue
+
+    d1=$(date -d "$exp" +%s 2>/dev/null) || continue
     d2=$(date -d "$now" +%s)
     exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-        # ambil block client vmess
+
+    if [ "$exp2" -le 0 ]; then
+        # Ambil block client VMESS untuk marker ini
         block=$(sed -n "/^### $user $exp/,/^},{/p" /etc/xray/config.json)
-        # ambil UUID (field \"id\")
+
+        # Ambil UUID (field "id")
         uuid=$(echo "$block" | grep -m1 '"id"' | cut -d'"' -f4)
 
-        # simpan ke expired.txt kalau uuid ketemu
+        # Simpan ke expired.txt
         if [ -n "$uuid" ]; then
             echo "$now vmess $user $uuid $exp" >> "$EXPIRED_FILE"
         fi
 
+        # Hapus user dari config.json
         sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
-        sed -i "/^### $user $exp/,/^},{/d" /etc/xray/config.json
-        rm -f /etc/xray/$user-tls.json /etc/xray/$user-none.json
     fi
 done
 
-#----- Auto Remove L2TP (TIDAK DIUBAH)
-data=( `cat /var/lib/crot/data-user-l2tp | grep '^###' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-    exp=$(grep -w "^### $user" "/var/lib/crot/data-user-l2tp" | cut -d ' ' -f 3)
-    d1=$(date -d "$exp" +%s)
-    d2=$(date -d "$now" +%s)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" = "0" ]]; then
-        sed -i "/^### $user $exp/d" "/var/lib/crot/data-user-l2tp"
-        sed -i '/^"'"$user"'" l2tpd/d' /etc/ppp/chap-secrets
-        sed -i '/^'"$user"':\$1\$/d' /etc/ipsec.d/passwd
-        chmod 600 /etc/ppp/chap-secrets* /etc/ipsec.d/passwd*
-    fi
-done
+############################
+# AUTO REMOVE VLESS (#&)
+############################
+grep -E '^#& ' /etc/xray/config.json | while read -r marker user exp; do
+    [ -z "$user" ] && continue
+    [ -z "$exp" ] && continue
 
-#----- Auto Remove PPTP (TIDAK DIUBAH)
-data=( `cat /var/lib/crot/data-user-pptp | grep '^###' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-    exp=$(grep -w "^### $user" "/var/lib/crot/data-user-pptp" | cut -d ' ' -f 3)
-    d1=$(date -d "$exp" +%s)
+    d1=$(date -d "$exp" +%s 2>/dev/null) || continue
     d2=$(date -d "$now" +%s)
     exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" = "0" ]]; then
-        sed -i "/^### $user $exp/d" "/var/lib/crot/data-user-pptp"
-        sed -i '/^"'"$user"'" pptpd/d' /etc/ppp/chap-secrets
-        chmod 600 /etc/ppp/chap-secrets*
-    fi
-done
 
-#----- Auto Remove SSTP (TIDAK DIUBAH)
-data=( `cat /var/lib/crot/data-user-sstp | grep '^###' | cut -d ' ' -f 2`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-    exp=$(grep -w "^### $user" "/var/lib/crot/data-user-sstp" | cut -d ' ' -f 3)
-    d1=$(date -d "$exp" +%s)
-    d2=$(date -d "$now" +%s)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" = "0" ]]; then
-        sed -i "/^### $user $exp/d" "/var/lib/crot/data-user-sstp"
-        sed -i '/^'"$user"'/d' /home/sstp/sstp_account
-    fi
-done
-
-#----- Auto Remove Vless
-data=( `cat /etc/xray/config.json | grep '^#&' | cut -d ' ' -f 2 | sort | uniq`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-    exp=$(grep -w "^#& $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-    d1=$(date -d "$exp" +%s)
-    d2=$(date -d "$now" +%s)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-        # ambil block client vless
+    if [ "$exp2" -le 0 ]; then
         block=$(sed -n "/^#& $user $exp/,/^},{/p" /etc/xray/config.json)
         uuid=$(echo "$block" | grep -m1 '"id"' | cut -d'"' -f4)
 
@@ -97,34 +55,30 @@ do
         fi
 
         sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
-        sed -i "/^#& $user $exp/,/^},{/d" /etc/xray/config.json
     fi
 done
 
-#----- Auto Remove Trojan
-data=( `cat /etc/xray/config.json | grep '^#!' | cut -d ' ' -f 2 | sort | uniq`);
-now=`date +"%Y-%m-%d"`
-for user in "${data[@]}"
-do
-    exp=$(grep -w "^#! $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
-    d1=$(date -d "$exp" +%s)
+############################
+# AUTO REMOVE TROJAN (#!)
+############################
+grep -E '^#! ' /etc/xray/config.json | while read -r marker user exp; do
+    [ -z "$user" ] && continue
+    [ -z "$exp" ] && continue
+
+    d1=$(date -d "$exp" +%s 2>/dev/null) || continue
     d2=$(date -d "$now" +%s)
     exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-        # ambil block client trojan
+
+    if [ "$exp2" -le 0 ]; then
         block=$(sed -n "/^#! $user $exp/,/^},{/p" /etc/xray/config.json)
 
-        # trojan biasanya pakai "password", tapi tetap cek "id" dulu
-        uuid=$(echo "$block" | grep -m1 '"id"' | cut -d'"' -f4)
-        if [ -z "$uuid" ]; then
-            uuid=$(echo "$block" | grep -m1 '"password"' | cut -d'"' -f4)
-        fi
+        # Trojan pakai "password"
+        uuid=$(echo "$block" | grep -m1 '"password"' | cut -d'"' -f4)
 
         if [ -n "$uuid" ]; then
             echo "$now trojan $user $uuid $exp" >> "$EXPIRED_FILE"
         fi
 
-        sed -i "/^#! $user $exp/,/^},{/d" /etc/xray/config.json
         sed -i "/^#! $user $exp/,/^},{/d" /etc/xray/config.json
     fi
 done
