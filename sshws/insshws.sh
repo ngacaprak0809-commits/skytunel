6569805e201ed9a213ddf7eb10bad1276b0c5f00
ngacaprak0809-commits <ngacaprak0809@gmail.com>
2026-02-12 17:12:24 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -3,10 +3,10 @@ clear
 cd
 
 #Install Script Websocket-SSH golang
-wget -O /usr/local/bin/ws-stunnel.go https://autoscript.caliphdev.com/sshws/ws-stunnel.go
+wget -O /usr/local/bin/ws-stunnel.go https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/master/sshws/ws-stunnel.go
 /usr/local/go/bin/go build -o /usr/local/bin/ws-stunnel /usr/local/bin/ws-stunnel.go
 chmod +x /usr/local/bin/ws-stunnel
-wget -O /etc/systemd/system/ws-stunnel.service https://autoscript.caliphdev.com/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-stunnel.service ttps://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/master/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 
 
 #restart service
