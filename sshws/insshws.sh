1459734211b6eefdfc9d3e7c66a5534142f6f8a3
ngacaprak0809-commits <ngacaprak0809@gmail.com>
2026-02-12 17:52:07 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -11,7 +11,7 @@ chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
 
 #System Dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/ws-dropbear.service wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/master/sshws/ws-dropbear.service && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/master/sshws/ws-dropbear.service && chmod +x /etc/systemd/system/ws-dropbear.service
 
 #System SSL/TLS Websocket-SSH Python
 wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/master/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
