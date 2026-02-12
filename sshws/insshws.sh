46dd9ff6d18824cd4cabbf73aa8719b1e3fa2122
ngacaprak0809-commits <ngacaprak0809@gmail.com>
2026-02-12 17:19:28 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -2,15 +2,28 @@
 clear
 cd
 
-#Install Script Websocket-SSH golang
-wget -O /usr/local/bin/ws-stunnel.go https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/master/sshws/ws-stunnel.go
-/usr/local/go/bin/go build -o /usr/local/bin/ws-stunnel /usr/local/bin/ws-stunnel.go
+#Install Script Websocket-SSH Python
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/master/sshws/ws-dropbear
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/master/sshws/ws-stunnel
+
+#izin permision
+chmod +x /usr/local/bin/ws-dropbear
 chmod +x /usr/local/bin/ws-stunnel
-wget -O /etc/systemd/system/ws-stunnel.service ttps://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/master/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
+
+#System Dropbear Websocket-SSH Python
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/master/sshws/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
+
+#System SSL/TLS Websocket-SSH Python
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/master/sshws/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
 
 
 #restart service
 systemctl daemon-reload
+
+#Enable & Start & Restart ws-dropbear service
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
 
 #Enable & Start & Restart ws-openssh service
 systemctl enable ws-stunnel.service
