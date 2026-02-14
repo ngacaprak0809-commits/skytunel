3a1839d2e758b0b4ac29b71b2225e924b3410957
ngacaprak0809-commits <ngacaprak0809@gmail.com>
2026-02-15 00:03:54 +0700

    Update speedtestfix.sh

--- fix/speedtestfix.sh
+++ fix/speedtestfix.sh
@@ -5,7 +5,7 @@ NC='\e[0m'
 clear
 rm -rf /usr/bin/speedtest
 echo -e "\e[1;32m Start download speedtest.. \e[0m"
-wget -q -O /usr/bin/speedtest https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/fix/speedtest.sh
+wget -q -O /usr/bin/speedtest https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/main/fix/speedtest.sh
 chmod +x /usr/bin/speedtest
 clear
 echo -e "\e[1;32m Setup done Please wait.. \e[0m"
