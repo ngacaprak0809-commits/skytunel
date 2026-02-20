4a5852a3dd9de104286445de705efa9fc5343d4b
ngacaprak0809-commits <ngacaprak0809@gmail.com>
2026-02-20 19:45:51 +0700

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -7,7 +7,7 @@ echo ""
 echo "Update Script..."
 
 # Define base URL
-BASE_URL="https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/main"
+BASE_URL="https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/main"
 
 # Function to download and install a file
 update_file() {
