4aee72c705fc85a2f2f0341de7c19be805eb2641
ngacaprak0809-commits <ngacaprak0809@gmail.com>
2026-02-28 09:52:34 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -188,6 +188,61 @@ sleep 0.5
 clear
 wget https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/main/websocket/websocket.sh && chmod +x websocket.sh && ./websocket.sh
 clear
+#!/bin/bash
+
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\E[0;100;33m      • TROJAN MENU •          \E[0m"
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+echo -e " [\e[36m•1\e[0m] Create Account Trojan "
+echo -e " [\e[36m•2\e[0m] Trial Account Trojan "
+echo -e " [\e[36m•3\e[0m] Extending Account Trojan "
+echo -e " [\e[36m•4\e[0m] Delete Account Trojan "
+echo -e " [\e[36m•5\e[0m] Check User Login Trojan "
+echo -e " [\e[36m•6\e[0m] User list created Account "
+echo -e ""
+echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
+echo -e   ""
+echo -e   "Press x or [ Ctrl+C ] • To-Exit"
+echo ""
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e ""
+read -p " Select menu : " opt
+echo -e ""
+case $opt in
+1) clear ; add-tr ;;
+2) clear ; trialtrojan ;;
+3) clear ; renew-tr ;;
+4) clear ; del-tr ;;
+5) clear ; cek-tr ;;
+6) clear ;
+   if [ -s /etc/log-create-trojan.log ]; then
+     awk '
+       /Remarks[[:space:]]*:/ {
+         gsub(/\033\[[0-9;]*[A-Za-z]/,"",$0);
+         gsub(/.*:[[:space:]]*/,"",$0);
+         remark=$0;
+       }
+       /Expired On[[:space:]]*:/ {
+         gsub(/\033\[[0-9;]*[A-Za-z]/,"",$0);
+         gsub(/.*:[[:space:]]*/,"",$0);
+         if(remark!=""){
+           printf "%-25s %s\n", remark, $0;
+           remark="";
+         }
+       }
+     ' /etc/log-create-trojan.log | column -t
+   else
+     echo "Log Trojan kosong."
+   fi
+   echo ""
+   read -n 1 -s -r -p "Press any key to back on menu"
+   exit ;;
+0) clear ; menu ;;
+x) exit ;;
+*) echo "Anda Salah Tekan" ; sleep 1 ; m-trojan ;;
+esac
+clear
 # Install backup/restore tools and updated system menu
 wget -O /usr/bin/backup.sh https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/main/menu/backup.sh
 wget -O /usr/bin/restore.sh https://raw.githubusercontent.com/ngacaprak0809-commits/skytunel/refs/heads/main/menu/restore.sh
