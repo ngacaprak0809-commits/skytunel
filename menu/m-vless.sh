4ec56a802f15bc8ef6dc378472b8fbba1f587019
ngacaprak0809-commits <ngacaprak0809@gmail.com>
2026-02-28 09:51:06 +0700

    Update m-vless.sh

--- menu/m-vless.sh
+++ menu/m-vless.sh
@@ -25,7 +25,29 @@ case $opt in
 3) clear ; renew-vless ; exit ;;
 4) clear ; del-vless ; exit ;;
 5) clear ; cek-vless ; exit ;;
-6) clear ; cat /etc/log-create-vless.log ; exit ;;
+6) clear ;
+   if [ -s /etc/log-create-vless.log ]; then
+     awk '
+       /Remarks[[:space:]]*:/ {
+         gsub(/\033\[[0-9;]*[A-Za-z]/,"",$0);
+         gsub(/.*:[[:space:]]*/,"",$0);
+         remark=$0;
+       }
+       /Expired On[[:space:]]*:/ {
+         gsub(/\033\[[0-9;]*[A-Za-z]/,"",$0);
+         gsub(/.*:[[:space:]]*/,"",$0);
+         if(remark!=""){
+           printf "%-25s %s\n", remark, $0;
+           remark="";
+         }
+       }
+     ' /etc/log-create-vless.log | column -t
+   else
+     echo "Log VLESS kosong."
+   fi
+   echo ""
+   read -n 1 -s -r -p "Press any key to back on menu"
+   exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; m-sshovpn ;;
